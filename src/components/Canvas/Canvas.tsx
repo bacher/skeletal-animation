fdddbf3337af3062bede0ffd28f98412e104a717
Ilya Lebedev <bacherko@gmail.com>
2021-12-04 17:02:42 +0300

    add render 7

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { init, initGL } from './renderIndexed6';
+import { init, initGL } from './renderIndexed7';
 
 export function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
